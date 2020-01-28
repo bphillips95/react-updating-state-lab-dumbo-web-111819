e249fb38051758a538734467a6a9679407da9a28
Baruch Phillips <baruchphillips@gmail.com>
2020-01-28 17:16:32 -0500

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,20 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+    state = { 
+        timesClicked: 0
+    }
+        clickfunc = () => { 
+        this.setState(prevCount => ({
+            timesClicked: prevCount.timesClicked+1 
+        }))
+    }
+    render() {
+        return (
+            <div>
+                <button onClick={this.clickfunc}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,60 @@
 // Code YouTubeDebugger Component Here
+import React, { Component } from 'react'
+
+export default class YouTubeDebugger extends Component {
+    state = { 
+            errors: [],
+            user: null,
+            settings: {
+              bitrate: 8,
+              video: {
+                resolution: '1080p'
+              }
+            }
+    }
+    // changeResolution = () => { 
+    //     this.setState({ 
+    //         settings: { 
+    //             video: { 
+    //                 resolution: '720p'
+    //             }
+    //         }
+    //     })
+    // }
+
+    // changeBitrate = () => { 
+    //     this.setState({ 
+    //         settings: { 
+    //             bitrate: 12
+    //         }         
+    //     })
+    // }
+    changeBitrate = () => {
+        this.setState({
+          settings: {
+            ...this.state.settings,
+            bitrate: 12
+          }
+        });
+      };
+    
+      changeResolution = () => {
+        this.setState({
+          settings: {
+            ...this.state.settings,
+            video: {
+              ...this.state.settings.video,
+              resolution: '720p'
+            }
+          }
+        });
+    }
+    render() {
+        return (
+            <div>
+    <button className='bitrate' onClick={this.changeBitrate}> {this.state.settings.bitrate}</button>
+        <button className='resolution' onClick={this.changeResolution}> {this.state.settings.video.resolution}</button>
+            </div>
+        )
+    }
+}
